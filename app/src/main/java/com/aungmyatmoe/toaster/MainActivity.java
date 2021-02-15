01f9f854abf7a3a42ee803f06ce54ecb3cb2de2a
amm834 <aungmyatmoe834@gmail.com>
2021-02-15 09:36:20 +0630

    Make Show Toast

--- app/src/main/java/com/aungmyatmoe/toaster/MainActivity.java
+++ app/src/main/java/com/aungmyatmoe/toaster/MainActivity.java
@@ -2,6 +2,8 @@ package com.aungmyatmoe.toaster;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.view.*;
 
 public class MainActivity extends Activity 
 {
@@ -11,4 +13,8 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	public void sayHello(View v){
+		Toast.makeText(this,"Hello My Lovely User",
+		5000).show();
+	}
 }
